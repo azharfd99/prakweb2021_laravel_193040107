b2c90d763f35b21a37cdab4e10644bb3e44ebcfa
Azhar Fiqri Dwiyana <58680939+azharfd99@users.noreply.github.com>
2021-10-07 23:29:21 +0700

    Menyelesaikan codingan pada video 3 dengan laravel

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
+});
+
+Route::get('/about', function () {
+    return view ('about', [
+        "name" => "Azhar Fiqri Dwiyana",
+        "email" => "azharfd@gmail.com" ,
+        "image" => "Azhar.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view ('posts');
 });
