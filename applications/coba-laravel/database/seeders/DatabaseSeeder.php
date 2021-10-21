d8b37774bad2fd19faac7859f06d97111d2b9f0c
Azhar Fiqri Dwiyana <58680939+azharfd99@users.noreply.github.com>
2021-10-21 19:11:37 +0700

    Menyelesaikan video 9 mengenai Database Seeder

--- applications/coba-laravel/database/seeders/DatabaseSeeder.php
+++ applications/coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,65 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Azhar Fiqri Dwiyana',
+            'email' => 'azharfd@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+        
+        User::create([
+            'name' => 'Kurosaki Ichigo',
+            'email' => 'ichigo@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nulla repudiandae dolore enim laboriosam excepturi alias ducimus, adipisci voluptatum est reprehenderit sunt odio reiciendis suscipit? Voluptatibus consequatur velit amet,' ,
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vitae harum quisquam, officia accusantium nam at tempore similique neque error illo dolores quidem maiores iste corporis. Est non repellendus, illo consequatur molestiae ut doloremque dolores earum ex voluptas harum ipsam deserunt optio numquam soluta quia porro laboriosam. Nisi dolore ut quae, nulla unde eligendi tenetur sapiente velit enim mollitia aut, doloremque esse. Distinctio qui autem eos nobis facilis ex maiores officiis nemo! Quibusdam pariatur quod illo molestias deserunt veniam unde at ipsa! Atque architecto aliquam tempora provident quae ex reprehenderit iusto sed fugit ratione amet voluptas, aut doloremque sint sit!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nulla repudiandae dolore enim laboriosam excepturi alias ducimus, adipisci voluptatum est reprehenderit sunt odio reiciendis suscipit? Voluptatibus consequatur velit amet,' ,
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vitae harum quisquam, officia accusantium nam at tempore similique neque error illo dolores quidem maiores iste corporis. Est non repellendus, illo consequatur molestiae ut doloremque dolores earum ex voluptas harum ipsam deserunt optio numquam soluta quia porro laboriosam. Nisi dolore ut quae, nulla unde eligendi tenetur sapiente velit enim mollitia aut, doloremque esse. Distinctio qui autem eos nobis facilis ex maiores officiis nemo! Quibusdam pariatur quod illo molestias deserunt veniam unde at ipsa! Atque architecto aliquam tempora provident quae ex reprehenderit iusto sed fugit ratione amet voluptas, aut doloremque sint sit!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nulla repudiandae dolore enim laboriosam excepturi alias ducimus, adipisci voluptatum est reprehenderit sunt odio reiciendis suscipit? Voluptatibus consequatur velit amet,' ,
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vitae harum quisquam, officia accusantium nam at tempore similique neque error illo dolores quidem maiores iste corporis. Est non repellendus, illo consequatur molestiae ut doloremque dolores earum ex voluptas harum ipsam deserunt optio numquam soluta quia porro laboriosam. Nisi dolore ut quae, nulla unde eligendi tenetur sapiente velit enim mollitia aut, doloremque esse. Distinctio qui autem eos nobis facilis ex maiores officiis nemo! Quibusdam pariatur quod illo molestias deserunt veniam unde at ipsa! Atque architecto aliquam tempora provident quae ex reprehenderit iusto sed fugit ratione amet voluptas, aut doloremque sint sit!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nulla repudiandae dolore enim laboriosam excepturi alias ducimus, adipisci voluptatum est reprehenderit sunt odio reiciendis suscipit? Voluptatibus consequatur velit amet,' ,
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vitae harum quisquam, officia accusantium nam at tempore similique neque error illo dolores quidem maiores iste corporis. Est non repellendus, illo consequatur molestiae ut doloremque dolores earum ex voluptas harum ipsam deserunt optio numquam soluta quia porro laboriosam. Nisi dolore ut quae, nulla unde eligendi tenetur sapiente velit enim mollitia aut, doloremque esse. Distinctio qui autem eos nobis facilis ex maiores officiis nemo! Quibusdam pariatur quod illo molestias deserunt veniam unde at ipsa! Atque architecto aliquam tempora provident quae ex reprehenderit iusto sed fugit ratione amet voluptas, aut doloremque sint sit!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- applications/coba-laravel/resources/views/post.blade.php
+++ applications/coba-laravel/resources/views/post.blade.php
@@ -3,12 +3,12 @@
 
 @section('container')
     <article>
-        <h2>{{ $post->title }}</h2>
+        <h1 class="mb-5">{{ $post->title }}</h1>
 
-        <p>By. Azhar Fiqri Dwiyana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
     </article>
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
